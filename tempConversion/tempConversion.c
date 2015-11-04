b456ecb64c01e9ff565a72159c73c0ad775e07f9
KPicart <kpicart@stepwisedesigns.com>
2015-11-03 23:46:35 -0500

    zzz

--- tempConversion/tempConversion.c
+++ tempConversion/tempConversion.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-main(){
-int fahr, celsius;
-int lower, upper, step;
+    main(){
+        int fahr, celsius;
+        int lower, upper, step;
 
-lower = 0;
-upper = 300;
-step = 20;
+        lower = 0;	/*lower limit temp scale */
+        upper = 300;	/* upper limit */
+        step = 20;	/* step size */
 
-fahr = 0;
-while (fahr <= upper){
-celsius = 5*(fahr-32)/9;
-printf("%d\t %d\n",fahr, celsius);
-fahr = fahr + step;
-}
-}
+        fahr = 0;
+        while (fahr <= upper){
+            celsius = 5*(fahr-32)/9;
+            printf("%3d\t %6d\n",fahr, celsius);
+            fahr = fahr + step;
+        }
+    }
