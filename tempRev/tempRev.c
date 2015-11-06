3a4e3e89f94598c967883ae8217838082ae67df1
KPicart <kpicart@stepwisedesigns.com>
2015-11-05 23:13:19 -0500

    tempRev output and intro #define (symbolic constants

---
+++ tempRev/tempRev.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+	#define UPPER 300
+	#define LOWER 0
+	#define STEP 20
+
+	int fahr;
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+	printf ("%3d  %6.1f\n", fahr,(5.0/9.0)*(fahr -STEP));
+}
