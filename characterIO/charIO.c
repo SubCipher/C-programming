b1ab5ec0e2ca4ca99e1a7dfc0589a9732d56c78d
KPicart <kpicart@stepwisedesigns.com>
2015-11-07 22:30:39 -0500

    working with getchar()

--- characterIO/charIO.c
+++ characterIO/charIO.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 
-#include <stdnio.h>
 	/* copy input to out put; 1st version */
 	main(){
 		int c;
-		while (c!= EOF) {
-			putchar(c);
-			c = getchar();
-		}
+		printf("%d\n", c);
+
+	 c = getchar();
+	printf ("%d\n",c);
 	}
