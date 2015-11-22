52696fb297f313191276c227b4745e7c2651b876
KPicart <kpicart@stepwisedesigns.com>
2015-11-22 00:30:25 -0500

    got it

--- getchar/temp.c
+++ getchar/temp.c
@@ -3,12 +3,15 @@
 
 main(){
 	int  x = 'A' + 'B';
+	
 	printf("hello world");
 	int y = 'b';	
 	
 	printf("\n %d \n",x);
-	printf("\n %d \n %d \n",'A','B');
-	printf("\n %d \n",'y');
+	
+	printf("%d \n %d \n",'A','B');
+	
+	printf("%d \n",'y');
 
 	
 }
