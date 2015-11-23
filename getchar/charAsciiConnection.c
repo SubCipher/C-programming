a502a93541411c58f7e0ad3823012bc17d93e833
KPicart <kpicart@stepwisedesigns.com>
2015-11-22 22:06:17 -0500

    char ascii connection

---
+++ getchar/charAsciiConnection.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+main(){
+	int  x = 'A' + 'B';
+	
+	printf("hello world");
+	int y = 'b';	
+	
+	printf("\n %d \n",x);
+	
+	printf("%d \n %d \n",'A','B');
+	
+	printf("%d \n",'y');
+
+	
+}
