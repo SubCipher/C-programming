6b5cbda43cce4cb50da00b5f6314a355485978c9
KPicart <kpicart@stepwisedesigns.com>
2015-11-23 23:59:31 -0500

    counting up

--- getchar/temp.c
+++ getchar/temp.c
@@ -2,16 +2,12 @@
 #include <stdbool.h>
 
 main(){
-	int  x = 'A' + 'B';
 	
-	printf("hello world");
-	int y = 'b';	
-	
-	printf("\n %d \n",x);
-	
-	printf("%d \n %d \n",'A','B');
-	
-	printf("%d \n",'y');
+	int a;
+	int z;
+	while (a <= z);
+	printf("%d",a);
+	++a;
 
 	
 }
