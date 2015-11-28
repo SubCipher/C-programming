ec483d4888c1e1e85a6ca35af4a790d1fd827657
KPicart <kpicart@stepwisedesigns.com>
2015-11-28 02:03:30 -0500

    don't stop, git,git,git

--- getchar/temp.c
+++ getchar/temp.c
@@ -3,11 +3,11 @@
 
 main(){
 
-	int a_count = 'a';
+	int  a_count = 'a';
 	int z_count = 'z';
 
 	while (a_count <= z_count){ 	
-	printf("%d\n",putchar(a_count));
+	printf("%c\n",a_count);
 	++a_count;
  }
 
