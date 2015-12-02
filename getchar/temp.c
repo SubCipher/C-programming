21fafc00175b441c4c355faf3a40ae61f28c2b96
KPicart <kpicart@stepwisedesigns.com>
2015-12-01 22:51:56 -0500

    working with getchar & EOF

--- getchar/temp.c
+++ getchar/temp.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 main(){
-
+	int c;
+	int d;
 
 	printf("the value of EOF %d \n", EOF);
 
+	printf("\nthe value  (c = getchar()) != EOF) is %d \n",(c = getchar()) != EOF);
+	printf("\nthe value  d  = getchar() != EOF is %d\n",d = getchar() != EOF);
 }
 
