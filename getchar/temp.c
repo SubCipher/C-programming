198b67345b39e042cff304f8fbbf56b9cc7b1078
KPicart <kpicart@stepwisedesigns.com>
2015-12-02 22:56:10 -0500

    pushing it forward

--- getchar/temp.c
+++ getchar/temp.c
@@ -8,5 +8,10 @@ main(){
 
 	printf("\nthe value  (c = getchar()) != EOF) is %d \n",(c = getchar()) != EOF);
 	printf("\nthe value  d  = getchar() != EOF is %d\n",d = getchar() != EOF);
+	
+	printf("The expression getchar() != EOF evaluates to %d\n", getchar() != EOF);
+	printf("The expression getchar() != EOF evaluates to %d\n", getchar() != EOF);	
+
+return 0;
 }
 
