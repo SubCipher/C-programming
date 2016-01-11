bcf0561c173e7ef208fdd5d5571dfeb557f20f3f
KPicart <kpicart@stepwisedesigns.com>
2016-01-10 23:17:19 -0500

    commited to arrays--dontStop

--- KnR/c1_ex13.c
+++ KnR/c1_ex13.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+
 int main(){
 	
-	int c,i;
-	
-	int letterCount[10];
+	int c,i,bSpace, tSpace,nLine;
+	c = 0;
+	int numOfLetters;
+	numOfLetters = 0;
+	int letterCount[10];			
 
-	for(i = 0; i < 9; i++)
-		++letterCount[i];
-	printf("letterCount %d", letterCount[i]);
+	nLine = tSpace = bSpace = numOfLetters;
 
+	for(i = 0; i < 10; i++)
+		letterCount[i] = 0;
+
+	while ((c = getchar()) != EOF){
+		if (c != ' ' || c != '\t' || c != '\n')
+		numOfLetters++;
+		else 
+			( letterCount[i] = numOfLetters);
+			numOfLetters = 0;	
+
+		}		
+	printf("number of letters array  %d" , letterCount[0]);
 
 }
