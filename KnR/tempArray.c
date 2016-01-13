2542e4817d3fc3aad565dab207246d05b40f506e
KPicart <kpicart@stepwisedesigns.com>
2016-01-12 23:28:41 -0500

    hip-hop ArrayHoArrayHo

--- KnR/tempArray.c
+++ KnR/tempArray.c
@@ -4,16 +4,15 @@ int main(){
 
 	int i, c;
 	int numArray[10];
-	c = 0;
-	for(i = 0; i < 10; i++)
-		numArray[i] = 0;
 	
 	while ((c = getchar()) != EOF){
 		
 		for(i = 0; i < 10; i++){
 			numArray[i] = c++;
-		printf("\n numArray %d\n", numArray[i]);
-		}
+
+		printf("numArray %d\n", numArray[i]);
+
+		}	
 	}
 
 }
