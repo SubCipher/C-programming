364e62efabac71ddb55dbe30cf2b1cb4096e8246
KPicart <kpicart@stepwisedesigns.com>
2016-01-16 22:52:56 -0500

    workITout

--- KnR/c1_ex14.c
+++ KnR/c1_ex14.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+
+
+
 int main(){
 
+	int i, c, nw, nc, state, wlen;
+	int numOfLetters[15];
+	state = OUT;
+	nw = nc = 0;
+
+	for (i = 0; i < 15; ++i)
+		numOfLetters[i] = 0;
+
+	while ((c = getchar()) != EOF) {
+
+	++nc;
+
+	if (c == ' ' || c == '\n' || c == '\t')
+		state = OUT;
+
+	else if (state == OUT) {
+		state = IN;
+		++nw;
+		nc = 0;
+		
+		}
+		numOfLetters[nw-1] = nc;
+	
+		}
+	printf("%d %d\n numOfLetters= %d\n", nw, nc,numOfLetters[nw]);
+	for (i = 0;  i < 15; ++i)
+	wlen = 0;
+
+
 }
+
+
