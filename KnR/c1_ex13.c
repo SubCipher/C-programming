6470e61fa5559d590d6c507fb3e973d61b5b4139
KPicart <kpicart@stepwisedesigns.com>
2016-01-24 23:18:40 -0500

    two arrays walk into a bar...

--- KnR/c1_ex13.c
+++ KnR/c1_ex13.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 
-
-
-
 int main(){
 	
-	int c,i,l;
-	c = 0;
-	int wordLen;
-	wordLen =  0;
-	l = 0;
-	int wordLetterCount[10];			
+	int i;
+	int c;
+	int wordLen = 0;
+	int l = 0;
 
+
+	int wordLetterCount[10];			
 	for(i = 0; i < 10; ++i)
-		wordLetterCount[i] = 0;
+		wordLetterCount[i] = 9;
 
 	while ((c = getchar()) != EOF){
 		if (c != ' ' || c != '\t' || c != '\n')
 			++wordLen;
-		else 
-		wordLetterCount[l] = wordLen;
+
+		else if ( c == ' ' || c == '\t' || c == '\n')
+			 wordLetterCount[l] = wordLen;
 			wordLen = 0;	
-			l++;
-		for( i = 0; i < 10; ++i);
-		printf(" %d", wordLetterCount[i]);
-
-	}
-
+			++l;
+		}
+	for( i = 0; i < 10; ++i)
+		printf(" %d\n", wordLetterCount[i]);
+		
 }
