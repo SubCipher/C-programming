021317f2569488c0903212a5159a737a8b793581
KPicart <kpicart@stepwisedesigns.com>
2016-01-25 23:59:27 -0500

    themRAYS

--- KnR/c1_ex13.c
+++ KnR/c1_ex13.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 
 int main(){
-	
+
+
+	// declare variable
 	int i;
 	int c;
 	int wordLen = 0;
 	int l = 0;
 
-
-	int wordLetterCount[10];			
+	// declare an array with capacity of 10 
+	int wordLetterCount[10];		
+	
+	// initialize array with for loop and val of 0 
 	for(i = 0; i < 10; ++i)
 		wordLetterCount[i] = 0;
 
+
+	// check for white spaces 
 	while ((c = getchar()) != EOF){
 		if (c != ' ' || c != '\t' || c != '\n'){
+
+	// if there is no white space keep count characters 
 			++wordLen;
 			}
 
+		//check for white spaces 
 		if ( c == ' ' || c == '\t' || c == '\n'){
+
+
+		// if white spaces found, add val of wordLen to array
 			for(i = 0; i < 10; ++i)
 			wordLetterCount[l] = wordLen;
-			wordLen = 0;	
+
+		//reset wordLen 
+			wordLen = 0;
+
+		//increase val of l to move to next position in array
 			++l;
 		}
 	}
+
+
+		//print results with for loop
 	for( i = 0; i < 10; ++i)
-		if (wordLetterCount[i] < 0)
-			printf(" *\n");
+		if (wordLetterCount[i] > 0)
+		printf("the number of letters in word %d is %d\n", i,wordLetterCount[i]);
 			
 			
 	
