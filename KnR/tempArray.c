7c03385cebce842783f9a7d6eea509833c549412
KPicart <kpicart@stepwisedesigns.com>
2016-02-05 21:18:06 -0500

    ABC

--- KnR/tempArray.c
+++ KnR/tempArray.c
@@ -4,15 +4,31 @@ int main(){
 
 	int i, c;
 	int numArray[10];
+	int charCount= 0;
+	int loopCount = 0 ;	
+
+	for(i=0; i < 10; i++){
+		numArray[i] = 0;
+		}
 	
 	while ((c = getchar()) != EOF){
 		
-		for(i = 0; i < 10; i++){
-			numArray[i] = c++;
+		if(c != ' ' || c != '\n'){
+			charCount++;
+			}
 
-		printf("%d\n", numArray[i]);
-
-		}	
-	}
+		else if(c == ' ' || c == '\n'){
+			numArray[loopCount] = charCount;
+			charCount = 0;
+			loopCount++;
+		}
+		}
+		for(i =0; i < 10; i++){
+			if (numArray[i] > 0){
+			printf("array value = %d\n", numArray[i]);
+			}	
+		}
 	return 0;
+
+
 }
