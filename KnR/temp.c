3ee672ff5ec3e45580db751dd321e1a118fc4378
knax <knax@knaxs-iMac.local>
2016-02-12 22:32:39 -0500

    testWithTemp

--- KnR/temp.c
+++ KnR/temp.c
@@ -15,10 +15,10 @@ int main() {
 	while ((c = getchar()) != EOF) {
 		++nc;
 		
-		if(c ==\'n')
+		if(c == '\n')
 			++nl;
 		
-		if(c ==' ' || c == '\n' || c = '\t')
+		if(c == ' ' || c == '\n' || c == '\t')
 			state = OUT;
 		else if (state == OUT) {
 			state = IN;
