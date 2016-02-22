4e4a8fe3aff6959ca8a006455656e0853874ec3e
Subcipher <krishna@javaworkgroup.com>
2016-02-22 18:34:27 -0500

    formating histograms

--- KnR/c1_ex13a.c
+++ KnR/c1_ex13a.c
@@ -8,49 +8,67 @@ int main(){
 	int c;
 	int wordLen = 0;
 	int l = 0;
+	int j = 0;
 
-	// declare an array with capacity of 10 
+	// declare and initilize array
 	int wordLetterCount[10];		
 	
-	// initialize array with for loop and val of 0 
 	for(i = 0; i < 10; ++i)
 		wordLetterCount[i] = 0;
 
 
-	// check for white spaces 
+	// check if white spaces present or not
 	while ((c = getchar()) != EOF){
-		if (c != ' ' || c != '\t' || c != '\n'){
 
-	// if there is no white space keep count characters 
+		if (c != ' ' || c != '\t' || c != '\n')
+		{
+
+		// if no white space count characters 
 			++wordLen;
-			}
+		}
 
-		//check for white spaces 
-		if ( c == ' ' || c == '\t' || c == '\n'){
-
-
-		// if white spaces found, add val of wordLen to array
+		//if white spaces present  
+		if ( c == ' ' || c == '\t' || c == '\n')
+		{
 			for(i = 0; i < 10; ++i)
 			wordLetterCount[l] = wordLen;
 
-		//reset wordLen 
+			//reset wordLen 
 			wordLen = 0;
 
-		//increase val of l to move to next position in array
+			//increase val of l to move to next position in array
 			++l;
 		}
 	}
 
 
-		//print results with for loop
-	for( i = 0; i < 10; ++i)
-		if (wordLetterCount[i] > 0)
+/*		for(i = 0; i < 10; ++i){
+
+			printf("%d\n", wordLetterCount[i]);
+		}
+*/
+	//print results with for loop
+	for( i = 0; i < 10; ++i){
+		printf("%2d | ",i);
+		for(j = 0; j < wordLetterCount[i]; ++j)
+		putchar('*');
+	putchar ('\n');
+
+	}
+/*
+		for(j = 0; j < wordLetterCount[i]; ++j)
+			if(wordLetterCount[i] == j){
+			putchar('*');
+			putchar('\n');
+			}
+		}
+*/
+return 0;
 		
-			while (wordLetterCount[i] > 0){
-			printf("*");
-			if (wordLetterCount
-			wordLetterCount[i] = (wordLetterCount[i] -1);
-			
-	}			
 	
 }
+
+
+
+
+
