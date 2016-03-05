aaf3579aee213fd4437de6aa768ae793526f9866
Subcipher <krishna@javaworkgroup.com>
2016-03-04 23:55:14 -0500

    waxing

--- KnR/c1_ex14waxOff01.c
+++ KnR/c1_ex14waxOff01.c
@@ -3,6 +3,34 @@
 
 int main(void){
 
+	int freqArray[NUM_CHARS +1];
+	int c;
+	long currentCharVal = 0;
+	long numOccurancesCharVal = 0;
+	int indexID = 0;
 
 
+	for (indexID = 0; indexID <= NUM_CHARS; indexID++){
+		freqArray[indexID] = 0;
+		}
+
+	while ((c = getchar()) != EOF){
+		if( c < NUM_CHARS)
+			{
+			currentCharVal = ++freqArray[c];
+				if(currentCharVal > numOccurancesCharVal){
+					numOccurancesCharVal = currentCharVal;
+				}
+			}
+
+
+			else 
+			{
+				currentCharVal = ++freqArray[NUM_CHARS];
+				if(currentCharVal > numOccurancesCharVal)
+				{
+					numOccurancesCharVal = currentCharVal;
+				}
+			}
+		} 
 }
