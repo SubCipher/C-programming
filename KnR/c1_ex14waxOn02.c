9de799c650060ce531fb679c5baddcb64ee82d8c
Subcipher <krishna@javaworkgroup.com>
2016-03-09 23:56:53 -0500

    frequentFlyer

--- KnR/c1_ex14waxOn02.c
+++ KnR/c1_ex14waxOn02.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
-
+#define NUM_CHARS 256
 
 int main(void){
+
+	int c;
+	long currentCharVal;
+	long maxCharVal;
+	int indexID;
+
+	long freqArray[NUM_CHARS +1];
+
+	for(indexID = 0; indexID <= NUM_CHARS; indexID++)
+		{
+			freqArray[indexID] = 0;
+		}
+
+	while (( c = getchar()) != EOF)
+		{
+		if (c < NUM_CHARS)
+			{
+				currentCharVal = ++freqArray[c];
+				if(currentCharVal > maxCharVal)
+				{
+					maxCharVal = currentCharVal;
+				}
+			}
+			else
+			{
+				currentCharVal = ++freqArray[NUM_CHARS];
+				if(currentCharVal > maxCharVal)
+					{
+						maxCharVal = currentCharVal;
+					}
+				}
+
+		}
+
+	int i = 0;
+	for(i = 0; i <= NUM_CHARS; ++i)
+		{
+			if(freqArray[i] > 0)
+			{
+				printf(" \nletter: %c frequency: %d", freqArray[i], freqArray[i]);
+ 			}
+		}
 
 return 0;
 }
