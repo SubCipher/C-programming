f6c31f452dd8023166d52c32806f021c0f4adc72
Subcipher <krishna@javaworkgroup.com>
2016-03-12 23:56:50 -0500

    newAlphabet

--- KnR/temp.c
+++ KnR/temp.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
-#define NUM_CHARS 256
 
 int main(void){
 
 	int c;
-	long currentCharVal = 0;
-	long maxCharVal = 0;
-	int i;
-
-	long freqArray[NUM_CHARS + 1];
-	int indexID= 0;
-	
-
 	c = getchar();
-
-	for( indexID = 0 ; indexID < NUM_CHARS; indexID++){
-		freqArray[indexID]=0;
-		}
-	currentCharVal = ++freqArray[c];
-	if (freqArray[indexID] > 0){
-	printf("indexID=%d \n indexID array value= %d\n", indexID,freqArray[c]);
+	int i;
+	for(i = 0; i < 26; ++i)
+	{
+		printf("\n alphaNumeric value = %c, %d", c, c);
+		c = ++c;
 	}
-	
-/*
-	for (i = 0; i < NUM_CHARS; ++i)
-
-	{printf("arrayValues: %d   ",freqArray[i]);
-	}
-*/
-
 return 0;
 
 }
