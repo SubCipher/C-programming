f0a74c7e0b472736d5a9d6559981d01d6d4d2b09
Subcipher <krishna@javaworkgroup.com>
2016-03-25 22:54:29 -0400

    ABCing

--- KnR/c1_ex18.c
+++ KnR/c1_ex18.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(){
+#define MAXLINE
+
+int myGetline(char line[], int maxline);
+void copy(char to[], charfrom[]);
+
+
+int main(void){
+
+	int c, i;
+	int line[MAXLINE + 1];
+	
+	c = getchar();
+	for(i = 0; i < lim
 
 }
--- KnR/myGetLine.c
+++ KnR/myGetLine.c
@@ -24,25 +24,23 @@ int main()
 	return 0;
 	}
 
-/*
-	int myGetLine(char s[], int lim)
-	{
+int myGetLine(char s[], int lim){
+	
 	int c, i;
 	for(i = 0; i < lim -1 && (c=getchar()) != EOF && c != '\n'; ++i)
 		s[i] = c;
-	if (c =='\n')
-	{
+	if (c =='\n'){
+
 		s[i] = c;
 		++i;
 	}
 	s[i] = '\0';
-	return i;
-	}
+return i;
+}
 
-	void copy(char to[], char from[])
-	{
+	void copy(char to[], char from[]){
 	int i;
 	i = 0;
 	while ((to[i] = from[i]) != '\0')
 	++i;
-} */
+} 
