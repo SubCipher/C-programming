46f96d4856168115a3510cac6ce9119806214a5b
Subcipher <krishna@javaworkgroup.com>
2016-03-26 23:44:08 -0400

    working with getline()

--- KnR/c1_ex18.c
+++ KnR/c1_ex18.c
@@ -1,17 +1,51 @@
 #include <stdio.h>
 
-#define MAXLINE
+#define MAXLINE 1000
 
 int myGetline(char line[], int maxline);
-void copy(char to[], charfrom[]);
+void copy(char to[], char from[]);
 
 
-int main(void){
+int main(){
 
-	int c, i;
-	int line[MAXLINE + 1];
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
 	
-	c = getchar();
-	for(i = 0; i < lim
+	while((len = myGetLine(line, MAXLINE)) > 0)
+		if(len > max) {
+			max = len;
+			printf("max val: %d",max);
+			copy(longest, line);
+		}
+	if(max >0)
+		printf("%s", longest);
+	return 0;
 
+	}
+
+int myGetline(char s[],int lim){
+	
+	int c, i;
+	for(i = 0; lim -1 && (c = getchar()) != EOF && c != '\n'; ++i){
+		s[i] = c;
+	if(c =='\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	}
+return i;
 }
+
+void copy(char to[], char from[])
+	{
+		int i;
+		while((to[i] = from[i]) != '\0')
+		++i;
+	}
+
+
+
+	
