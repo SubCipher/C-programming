84fc8fc93b6c36bda633657319cc20d3432a7176
Subcipher <krishna@javaworkgroup.com>
2016-04-16 15:55:32 -0400

    ABC

--- KnR/temp.c
+++ KnR/temp.c
@@ -5,23 +5,23 @@
 int main(){
 
 	int c, wordCount;
-	int nonWordCount;
+	int nonWordCount = 0;
 
 	int indexID;
 	char s[MAXLIM];
-	for( indexID = 0; indexID < MAXLIM; ++indexID){
-		s[indexID] = 0;
+	/*for( indexID = 0; indexID < MAXLIM; ++indexID){
+		s[indexID] = 0; *
 
-		}
+		} */
 
 	while((c = getchar()) != EOF){
 	
-		if(c != ' ' || c != '\t'  || c != '\n'){
+		if(c != ' ' || c != '\t' ){
 			s[indexID] = c;
 			++indexID;
 			++wordCount;
 			}
-		if(c == ' ' || c == '\t' || c == '\n'){
+		if(c == ' ' || c == '\t'){
 			s[indexID] = c;
 			++nonWordCount;
 			++indexID;					
@@ -32,7 +32,7 @@ int main(){
 
 		int i = 0;	
 		printf("word count: %d  non-word count: %d", wordCount, nonWordCount);
-		for( i = 0; i < indexID -1; ++i){
+		for( i = 0; i < (wordCount + nonWordCount) ; ++i){
 			printf("%d", s[i]);
 		
 		}
