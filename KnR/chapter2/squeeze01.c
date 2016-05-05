7071b22a07f8abc9ce0d0acdd89b44bc16742836
Subcipher <krishna@javaworkgroup.com>
2016-05-04 22:16:12 -0400

    0.15

--- KnR/chapter2/squeeze01.c
+++ KnR/chapter2/squeeze01.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #define MAXLIMIT 1024
 
+struct my_values {
+
+	char v1[20];
+	char v2[20];
+
+};
+
+struct get_values {
+
+	char v1[20];
+	char v2[20];
+
+};
+
+
+
 char *squeezing( char s0[], char s1[]){
+
+	struct my_values value1;
 
 	int i,j;
 	for(i = j = 0; s0[i] == '\0'; i++){
@@ -10,7 +29,11 @@ char *squeezing( char s0[], char s1[]){
 	s0[j] = '\0';
 		}
 	
-	return s1,s0;
+	strcpy( value1.v1, s0);
+	strcpy( value1.v2, s1);
+
+
+	return 0;
 	}
 
 char * getText(){
@@ -32,6 +55,7 @@ char * getText(){
 }
 
 int main(void){
+	struct get_values value2;
 
 	int c;
 	int i;
@@ -49,8 +73,8 @@ int main(void){
 	}
 	//getFromGetText = getText(s2);
 
-	getS0,getS1 = squeezing(s2, getText());
+ 	getS0 = squeezing(s2, getText());
 	
-	printf("getS0 = %s, getS1 = %s \n",getS0,getS1);
+	printf("value2 = %s, %s", getS0.v1, value2.v2);
 
 }
