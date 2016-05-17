ff0e2442c6ba72cac9043e6c8d9c94cfc7455a74
Subcipher <krishna@javaworkgroup.com>
2016-05-16 22:47:30 -0400

    underStanding Data Types

--- KnR/chapter2/ex_dataTypes.c
+++ KnR/chapter2/ex_dataTypes.c
@@ -4,37 +4,40 @@
 
 int main(void){
 
-printf("***** SIGNED CHAR *****\n\n");
+printf("***** DATA TYPE: SIGNED CHAR *****\n\n");
 printf("Bits used for signed char: %lu\n",8 *sizeof(signed(char)));
 printf("Bytes used for signed char: %lu\n",sizeof(signed(char)));
 printf("Maximum value range of signed char: %d\n", SCHAR_MAX);
 printf("Minimum value range of signed char : %d\n\n", SCHAR_MIN);
 
-
-printf("***** UNSIGNED CHAR value range*****\n");
+printf("*********************** DATA TYPE: UNSIGNED CHAR ************************\n\n");
 printf("Bits used for unsigned char: %lu\n",8*sizeof(unsigned(char)));
 printf("Bytes used for unsiged char: %lu\n", sizeof(unsigned(char)));
-printf("type (unsigned)char max value (UCHAR_MAX): %d\n\n",UCHAR_MAX);
+printf("Max value for (unsigned)char (UCHAR_MAX): %d\n\n",UCHAR_MAX);
 
-printf("***** TYPE INT ******\n\n");
-printf("BIts used for signed int: %lu\n",8*sizeof(int));
-printf("Bytes used for signed int: %lu\n\n\n", sizeof(int));
+printf("************************ DATA TYPE: INT *****************************\n\n");
+printf("BIts used for signed int: %lu\n",8*sizeof(signed(int)));
+printf("Bytes used for signed int: %lu\n\n\n", sizeof(signed(int)));
 printf("Maximum value range of type signed int: %d\n", INT_MAX);
 printf("Minimum value range  of type signed int: %d\n\n", INT_MIN);
 
-printf("***** type short (short)int*****\n\n");
+printf("*************************** DATA TYPE: UNSIGNED int ***************************\n\n");
+
+printf("BIts used for unsigned int: %lu\n",8*(sizeof(unsigned(int))));
+printf("BIts used for unsigned int: %lu\n", sizeof(unsigned (int)));
+printf("Maximum value range of type unsigned int: %u\n\n", UINT_MAX);
+
+
+printf("************************* DATA TYPE: (short)int ***********************\n\n");
 printf("Bits used for short: %lu\n",8*sizeof(short));
 printf("Bytes used for short: %lu\n",sizeof(short));
 printf("Maximum value range of type short: %d\n", SHRT_MAX);
 printf("Minimum value range of type short: %d\n", SHRT_MIN);
 
-printf("*****Unsigned short*****\n");
+printf("*************************** DATA TYPE: Unsigned short ******************************\n");
 printf("Maximum value range of type unsigned short: %u\n\n",(unsigned) USHRT_MAX);
 
-printf("***** UNSIGNED int *UINT_MAX *****\n\n");
-printf("Maximum value range of type unsigned int: %u\n\n", UINT_MAX);
-
-printf("*****TYPE long *****\n\n");
+printf("**************************** DATA TYPE long *******************************************\n\n");
 printf("Bits used for type long: %ld\n",8*sizeof(long));
 printf("Bytes used for type long: %ld\n",sizeof(long));
 printf("Maximum value range of type long: %ld\n", LONG_MAX);
@@ -44,16 +47,27 @@ printf("***** TYPE unsigned long/ ULONG_MAX *****\n\n");
 printf("Maximum value range of type unsigned long: %lu\n\n", ULONG_MAX);
 
 
-printf("*****************Type Float********************\n\n");
+printf("************************************** DATA TYPE: Float **********************************\n\n");
 printf("Bits used for unsigned Float %lu\n",8*(sizeof(float)));
 printf("Bytes used for unsigned Float %lu\n",sizeof(float));
  
 
 
-printf("*****************Type Double******************\n\n");
+printf("**************************   DATA TYPE Unsigned Double **********************************\n\n");
 printf("Bits used for unsigned double %lu \n",8*(sizeof(unsigned(double)))); 
 printf("Bytes used for unsigned double %lu \n",sizeof(unsigned(double)));
+
+
+printf("********************************    DATA TYPE: Signed Double    ******************************\n\n");
+printf("Bits used for signed double %ld\n", 8*(sizeof(signed(double))));
+printf("Bytes used for signed double %ld\n", sizeof(signed(double)));
+
+
+
 
 return 0;
 
 }
+
+
+
