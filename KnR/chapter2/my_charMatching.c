07fddb91895eb041265e1389859b4f0c1e25dd31
Art <kpicart@stepwisedesigns.com>
2016-05-26 14:14:55 -0400

    2D array w/function and pointers: update

--- KnR/chapter2/my_charMatching.c
+++ KnR/chapter2/my_charMatching.c
@@ -3,12 +3,18 @@
 #include <stdlib.h>
 #define MAXLIMIT 1024
 
+
+/*define function*/ 
 char *function00(){
 
 	int i = 0;
 	int c;
+	/*declare pointer outToMain*/ 
 	char *outToMain;
+
+	/*allocate memory for pointer and return a pointer to it*/ 
 	outToMain =(char* )calloc(25,sizeof(char));
+	
 	while((c =getchar()) != EOF){
 		outToMain[i] = c;
 		++i;
@@ -19,26 +25,26 @@ char *function00(){
 int main(void){
 
 	int i;
+	/*declare 2D array*/
 	char s0[MAXLIMIT][50];	
+	
 	int j,numToCompare;
 	char *inFromFunction;
 	
 	printf("how many arrays are to be read?");
 	scanf("%d",&numToCompare);
-
-	inFromFunction = function00();
-	for(i =0; i < 5; i++){
-		printf("returned value %c\n",inFromFunction[i]);
-	} 
-
-	while(j < numToCompare-1){
-		for(i=0; i < (strlen(inFromFunction)-1); i++){
-			s0[0][i] = inFromFunction[i];
+	
+	//inFromFunction = function00();
+	
+		while(j < (numToCompare-1)){
+			inFromFunction = function00();
+			for(i=0; i < (strlen(inFromFunction)); i++){
+				s0[j][i] = inFromFunction[i];
 			}
 		j++;
+		}
+
+		printf("results: %s\n",s0[0]);
+	
+	return 0;
 	}
-
-	printf("results: %s\n",s0[0]);
-
-return 0;
-}
