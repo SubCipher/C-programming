ea10b423a53ef0906d52c020dab02d7510e82fc9
Subcipher <krishna@javaworkgroup.com>
2016-05-26 08:08:14 -0400

    using a pointer to a 2D array from a function

--- KnR/chapter2/my_charMatching.c
+++ KnR/chapter2/my_charMatching.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define MAXLIMIT 1024
 
-char* function00(){
+char *function00(){
 
 	int i = 0;
 	int c;
-	char outToMain[MAXLIMIT];
+	char *outToMain;
 	outToMain =(char* )calloc(25,sizeof(char));
-	while((c =getchar()) !='\n'){
-		outToMain[i++] = c;
-
+	while((c =getchar()) != EOF){
+		outToMain[i] = c;
+		++i;
 	}
-return outToMain;
-}
+	return outToMain;
+	}
  
-int main(){
+int main(void){
 
 	int i;
 	char s0[MAXLIMIT][50];	
@@ -26,12 +27,18 @@ int main(){
 	scanf("%d",&numToCompare);
 
 	inFromFunction = function00();
-	
+	for(i =0; i < 5; i++){
+		printf("returned value %c\n",inFromFunction[i]);
+	} 
+
 	while(j < numToCompare-1){
-		for(i=0; i < numToCompare-1; i++){
-			s0[0][j] = inFromFunction[i];
+		for(i=0; i < (strlen(inFromFunction)-1); i++){
+			s0[0][i] = inFromFunction[i];
 			}
 		j++;
 	}
+
+	printf("results: %s\n",s0[0]);
+
 return 0;
 }
