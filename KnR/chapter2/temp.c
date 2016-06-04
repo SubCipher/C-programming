8d2ef5f2065cd05b8ba501a3fe027f3b959cedb9
Art <kpicart@stepwisedesigns.com>
2016-06-03 23:35:20 -0400

    rosettaStone

--- KnR/chapter2/temp.c
+++ KnR/chapter2/temp.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define MAXLIMIT 1024
 char *getInput(n){
 	
-	int i, c;
+	int i=0;
+	int c;
 	char* s;
 	s = (char*)calloc(n,sizeof(char));
 	while((c = getchar()) != EOF){
@@ -14,42 +15,47 @@ char *getInput(n){
 		i++;
 		}
 
-	free(s);
+//	free(s);
 	return s;
 }
 
 int main(void){
 
-	int i=0,count,numArraysToCompare; 
+	int i=0;
+	int elementCount =0;
+	int numArraysToCompare =0; 
 	int j,x =0;	
+	int breakCount=0;	
 	char* inputText;
-	//char s[10][count];
-	printf("how much memory should we allocation for array elements?: \n");	
-	scanf("%d",&count);
+	printf("how much memory should we allocate for array elements?: \n");	
+	scanf("%d",&elementCount);
 	
 	printf("enter the amount of arrays to make for comparison: \n");
 	scanf("%d",&numArraysToCompare);
-	char* s[numArraysToCompare][count];
+	char* s[100][100];
 
-	printf("enter %d elements to store in memory: \n",count);
+	printf("enter %d elements to store in memory: \n",elementCount);
 	  
-	inputText = getInput(count);
-
-	for(j =0; j < strlen(inputText); j++){
-		s[i][j] = &inputText[j];
+	inputText = getInput(numArraysToCompare);
+	
+		for(j =0; j < strlen(inputText); j++){
+			s[i][j] = &inputText[j];
+			if(inputText[j] == '\n'){
+				++i;
 		}
-	for(j= x =0; x < strlen(inputText); x++){
-		printf("this is it: %c(%d) memory location: %p\n",s[i][j][x],x,&inputText[x]);
-		
 	}
 
+/*	 for(i =0; i<(numArraysToCompare); i++){
+		for(j= 0; j <strlen(inputText); j++){
+			for(x =0; x < strlen(inputText) ;++x){
+				printf("this is it:index[%d][%d][%d] | value: %c(%d)| memory location: %p\n",i,j,x,s[i][j][x],x,&inputText[x]);
+			}
+		}	
+	}
+A
+A
+*/	
 	printf("\n\n");
-	printf("what it is: %c | location in memory: %p \n",s[0][0][1], &s[0][0][1]);
-	printf("what it is: %c | location in memory: %p \n",s[0][0][2], &s[0][0][2]);
-	printf("what it is: %c | location in memory: %p \n",s[0][0][3], &s[0][0][3]);
-	printf("\n\n");
-
-
 return 0;
 
 }
