abd914938e192c9497889cab9383afc0d19f4798
Subcipher <krishna@javaworkgroup.com>
2016-06-13 13:13:41 -0400

    pointing everywhere

--- KnR/chapter2/ex_char01.c
+++ KnR/chapter2/ex_char01.c
@@ -5,18 +5,17 @@ int main(){
 
 	char word1[7] = "krishna";
 	char word2[10] = "homeTkex";
-	char newWord[2];
+	char newWord[10];
 	int i,j;
 	int k =0;
 	for(i =0; i<strlen(word1); i++){
 		for(j =0; j <strlen(word2); j++){
 			if(word1[i] == word2[j]){
-				newWord[k] = word1[i];
-				++k;
-				printf("the match is:%c\n\n",word2[j]);
-				
-				}
-		 }
-	}printf("newWord= %s\n",newWord);
+				printf("match, %c\n",word1[i] );
+				newWord[k++] = word1[i];
+			}	
+		}			
 
-}	
+	}	
+		printf("%s\n\n",newWord);
+}
