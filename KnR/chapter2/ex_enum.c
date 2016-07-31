d742ad29a38623541869d24f455782bd83a6886f
KPicart <kpicart@stepwisedesigns.com>
2016-07-30 22:43:03 -0400

    enum

---
+++ KnR/chapter2/ex_enum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+enum months {Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+enum months total;
+	total = 17 + Feb;
+
+	printf("total  value = %d:",total);
+
+}
+
